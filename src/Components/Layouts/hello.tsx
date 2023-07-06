f3cbbdb3c991eed72522f5d0af8d1c0d1d09d54f
Phinatech <ugbojudithchinemerem@gmil.com>
2023-07-07 00:04:03 +0100

    hello

---
+++ src/Components/Layouts/hello.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const hello = () => {
+  return (
+    <div>
+      <div>hello</div>
+      <div>hello</div>
+      <div>hello</div>
+      <div>hello</div>
+      <div>hello</div>
+    </div>
+  );
+}
+
+export default hello
